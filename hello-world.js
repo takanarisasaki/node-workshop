efd5e09f4cdb04afc3da706137710766f2e49c3e
Takanari Sasaki <takanari.s@hotmail.com>
2016-07-11 17:56:38 +0000

    Question 2: Printing every x second for infinite time

--- hello-world.js
+++ hello-world.js
@@ -1,5 +1,29 @@
+//Question 1: Using setTimeout
+console.log("Question 1:");
+
 console.log("Hello World!");
 
 setTimeout(function() {
     console.log("Hello World Again!!");
-}, 10000);
+}, 0);  //this should be 10000 for 10 seconds
+
+
+//Question 2: Mimicing setInterval function with setTimeout
+console.log("Question 2:");
+
+//Try to do this using setTimeout
+/*
+setInterval(function() {
+    console.log("Hello!");
+}, 2000);
+*/
+
+
+function callInfinitely() { 
+    setTimeout (function () {
+        console.log("Hello World!");
+        callInfinitely();
+    },2000);
+}
+
+callInfinitely();
